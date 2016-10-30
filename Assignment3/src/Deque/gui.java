7af69b1d3e517dfb7bfff07e1624787e0c2d6068
Arcolin <ericlarocque10@gmail.com>
2016-10-29 20:04:43 -0400

    update

--- Assignment3/src/Deque/gui.java
+++ Assignment3/src/Deque/gui.java
@@ -1,5 +1,7 @@
 package Deque;
 import javax.swing.*;
+import java.awt.event.*;
+import java.util.Deque;
 public class gui extends JFrame
 {
 	private JPanel panel;
@@ -18,17 +20,17 @@ public class gui extends JFrame
 	private final int WINDOW_WIDTH = 600;
 	private final int WINDOW_HEIGHT = 150;
 		
-	public gui()
+	public gui(dequeClass<String> deque)
 	{
 		setTitle("Assignment 3");
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		buildPanel();
+		buildPanel(deque);
 		add(panel);
 		setVisible(true);
 	}
 	
-	private void buildPanel()
+	private void buildPanel(dequeClass<String> deque)
 	{
 		addLabel = new JLabel("Enter something to add to the list:");
 		addField = new JTextField(10);
@@ -42,6 +44,17 @@ public class gui extends JFrame
 		isEmpty = new JButton("Empty?");
 		showFront = new JButton("Front Item");
 		showRear = new JButton("Rear Item");
+		
+		insertFrontButton.addActionListener(new InsertFrontListener());
+		insertRearButton.addActionListener(new InsertRearListener());
+		removeFrontButton.addActionListener(new RemoveFrontListener());
+		removeRearButton.addActionListener(new RemoveRearListener());
+		printList.addActionListener(new PrintListener());
+		clearList.addActionListener(new ClearListener());
+		size.addActionListener(new SizeListener());
+		isEmpty.addActionListener(new EmptyListener());
+		showFront.addActionListener(new FrontListner());
+		showRear.addActionListener(new RearListener());
 		
 		panel = new JPanel();
 		
@@ -60,11 +73,91 @@ public class gui extends JFrame
 		
 	}
 	
+	private  class InsertFrontListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			String input = addField.getText();
+			
+			addField.setText("");
+		}
+	}
+	
+	private class InsertRearListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			String input = addField.getText();
+			dequeClass<String> deque = new dequeClass<String>();
+			deque.insertRear(input);
+			addField.setText("");
+		}
+	}
+	
+	private class RemoveFrontListener<E> implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			dequeClass<E> deque = new dequeClass<E>();
+			deque.removeFront();
+		}
+	}
+
+	private class RemoveRearListener<E> implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			dequeClass<E> deque = new dequeClass<E>();
+			deque.removeRear();
+		}
+	}
+
+	private class PrintListener<E> implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			dequeClass<E> deque = new dequeClass<E>();
+			String printOut = deque.toString();
+			JOptionPane.showMessageDialog(null, printOut);
+		}
+	}
+
+	private class ClearListener<E> implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			dequeClass<E> deque = new dequeClass<E>();
+			deque.clear();
+		}
+	}
+
+	private class SizeListener<E> implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			dequeClass<E> deque = new dequeClass<E>();
+			int size = deque.size();
+			JOptionPane.showMessageDialog(null, size);
+		}
+	}
+
+	private class EmptyListener<E> implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			dequeClass<E> deque = new dequeClass<E>();
+			boolean result = deque.isEmpty();
+			if(result){
+				JOptionPane.showMessageDialog(null, "The list is empty");
+			}
+			JOptionPane.showMessageDialog(null, "There are "+ deque.size());
+		}
+	}
+
+	private class FrontListner<E> implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			dequeClass<E> deque = new dequeClass<E>();
+			E element = deque.getFront();
+			JOptionPane.showMessageDialog(null, "This is the current front object: " + element);
+		}
+	}
+
+	private class RearListener<E> implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			dequeClass<E> deque = new dequeClass<E>();
+			E element = deque.getRear();
+			JOptionPane.showMessageDialog(null, "This is the current rear object: " + element);
+		}
+	}
+	
 	public static void main(String[] args)
 	{
-		new gui();
-		
-		
+		dequeClass<String> deque = new dequeClass<String>();
+		new gui(deque);	
 	}
 
 }
